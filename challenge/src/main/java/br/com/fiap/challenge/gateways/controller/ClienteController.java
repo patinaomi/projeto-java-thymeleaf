4486382bede6dfd4ba8b0d6dee69b52796d5b4a0
Patricia Naomi <patricianaomiyama@gmail.com>
2025-03-19 16:21:39 -0300

    FEAT: Crud cliente finalizado

--- challenge/src/main/java/br/com/fiap/challenge/gateways/controller/ClienteController.java
+++ challenge/src/main/java/br/com/fiap/challenge/gateways/controller/ClienteController.java
@@ -1,15 +1,11 @@
 package br.com.fiap.challenge.gateways.controller;
 
 import br.com.fiap.challenge.domains.Cliente;
-import br.com.fiap.challenge.gateways.repository.ClienteRepository;
-import br.com.fiap.challenge.gateways.request.ClienteRequest;
 import br.com.fiap.challenge.service.ClienteService;
-import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.*;
-import org.springframework.web.servlet.ModelAndView;
 
 import java.util.List;
 
@@ -39,27 +35,22 @@ public class ClienteController {
         return "redirect:/clientes";
     }
 
-
-
-
-
-
-        @GetMapping("/formulario")
-    public ModelAndView carregarFormulario(@RequestParam(required = false) Integer id) {
-        Cliente cliente = (id != null) ? clienteService.buscarPorId(id) : new Cliente();
-        return new ModelAndView("clientes/formulario-cliente", "cliente", cliente);
+    @GetMapping("/editar/{id}")
+    public String getEditarCliente(@PathVariable Integer id, Model model) {
+        Cliente cliente = clienteService.buscarPorId(id);
+        model.addAttribute("clienteEdit", cliente);
+        return "edit_cliente_page";
     }
 
-
-    @GetMapping("/editar/{id}")
-    public ModelAndView editarCliente(@PathVariable Integer id) {
-        Cliente cliente = clienteService.buscarPorId(id);
-        return new ModelAndView("clientes/formulario-cliente", "cliente", cliente);
+    @PostMapping("/editar")
+    public String editarCliente(@RequestParam Integer id, @ModelAttribute Cliente cliente) {
+        clienteService.atualizar(id, cliente);
+        return "redirect:/clientes";
     }
 
     @GetMapping("/deletar/{id}")
-    public ModelAndView deletarCliente(@PathVariable Integer id) {
+    public String deletarCliente(@PathVariable Integer id) {
         clienteService.deletar(id);
-        return new ModelAndView("redirect:/clientes?sucesso");
+        return "redirect:/clientes";
     }
 }
