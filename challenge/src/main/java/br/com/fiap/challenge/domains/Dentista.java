bc8398e6543b33e09d357ef200b5f9625f4390f3
Patricia Naomi <patricianaomiyama@gmail.com>
2025-04-22 00:10:48 -0300

    FEAT: Adiciona Role nas classes de Clinica e Dentista

--- challenge/src/main/java/br/com/fiap/challenge/domains/Dentista.java
+++ challenge/src/main/java/br/com/fiap/challenge/domains/Dentista.java
@@ -1,5 +1,6 @@
 package br.com.fiap.challenge.domains;
 
+import br.com.fiap.challenge.domains.enums.Role;
 import jakarta.persistence.*;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
@@ -39,4 +40,14 @@ public class Dentista {
     @Column(name = "avaliacao", nullable = false)
     private Float avaliacao;
 
+    @Enumerated(EnumType.STRING)
+    private Integer role;
+
+    public Role getRole() {
+        return Role.toEnum(role);
+    }
+
+    public void setRole(Role role) {
+        this.role = role.getCod();
+    }
 }
